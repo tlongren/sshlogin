5a139584498f7dcfc4dec7b864aae2152da2c925
Tyler Longren <tylerl@saltechsystems.com>
2018-01-16 10:12:49 -0600

    💦 Renaming.

--- src/extension.ts
+++ src/extension.ts
@@ -10,14 +10,14 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sshlogin" is now active!');
+    console.log('Congratulations, your extension "SSH Terminal Login" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sshlogin', () => {
+    let disposable = vscode.commands.registerCommand('extension.ssh-terminal-login', () => {
 
-        let config = vscode.workspace.getConfiguration('sshlogin');
+        let config = vscode.workspace.getConfiguration('ssh-terminal-login');
 
         const connections = config.get<config[]>('connection');
 
